0b1040eb9295518d53f501d2344211c051d63fbf
Thales Rocha <thales_rochasousa@hotmail.com>
2023-08-19 23:25:29 -0300

    Add middleware para tratar corpo da requisição

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import http from "node:http";
 
+import { json } from "./middlewares/json.js";
+
 const server = http.createServer(async (req, res) => {
+  await json(req, res);
+
   return res.writeHead(404).end();
 });
 
