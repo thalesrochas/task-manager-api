790221741d5b6851d0eee04a9e3ff42e7927a7ed
Thales Rocha <thales_rochasousa@hotmail.com>
2023-08-19 23:37:37 -0300

    Add criação de uma task

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,29 @@
+import { randomUUID } from "node:crypto";
+
+import { Database } from "./database.js";
 import { buildRoutePath } from "./utils/buildRoutePath.js";
+
+const database = new Database();
 
 export const routes = [
   {
-    method: "GET",
-    path: buildRoutePath("/"),
-    handler: (_req, res) => {
-      return res.writeHead(200).end();
+    method: "POST",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const { title, description } = req.body;
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completedAt: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      database.insert("tasks", task);
+
+      return res.writeHead(201).end();
     },
   },
 ];
