e6d349fef0dfab4b54b1683fc26a957798f2828e
Thales Rocha <thales_rochasousa@hotmail.com>
2023-08-19 23:40:20 -0300

    Add remoção de task por id

--- src/routes.js
+++ src/routes.js
@@ -55,4 +55,15 @@ export const routes = [
       return res.writeHead(204).end();
     },
   },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {
+      const { id } = req.params;
+
+      database.delete("tasks", id);
+
+      return res.writeHead(204).end();
+    },
+  },
 ];
